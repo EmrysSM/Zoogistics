5d01cf5158edfd435acc8e99bbb6eaa70c00949e
escottmurrell <escottmurrell@hntb.com>
2018-10-15 15:20:50 -0400

    Started adding main classes for interacting with view

--- P2_zoo/src/p2_zoo/ZooController.java
+++ P2_zoo/src/p2_zoo/ZooController.java
@@ -1,4 +1,22 @@
 package p2_zoo;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class ZooController {
+    public ZooModel model;
+    public ZooView view;
+
+    public ZooController(ZooModel model, ZooView view) {
+        this.model = model;
+        this.view = view;
+    }
+
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    public void addListener(ActionListener l) {
+
+    }
 }
