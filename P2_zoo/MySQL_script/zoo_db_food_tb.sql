7e6ed99ba77f05335b4902a742b180bb6ed2c565
Dung Nguyen <42613647+D-Nguyen88@users.noreply.github.com>
2018-10-15 14:40:22 -0400

    Add files via upload

---
+++ P2_zoo/MySQL_script/zoo_db_food_tb.sql
@@ -0,0 +1,56 @@
+-- MySQL dump 10.13  Distrib 8.0.12, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: zoo_db
+-- ------------------------------------------------------
+-- Server version	8.0.12
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `food_tb`
+--
+
+DROP TABLE IF EXISTS `food_tb`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `food_tb` (
+  `main_tb_id` int(11) NOT NULL,
+  `Meat` tinyint(4) DEFAULT NULL,
+  `Bamboo` tinyint(4) DEFAULT NULL,
+  `Grass` tinyint(4) DEFAULT NULL,
+  `Fruit` tinyint(4) DEFAULT NULL,
+  `Vegetable` tinyint(4) DEFAULT NULL,
+  PRIMARY KEY (`main_tb_id`),
+  CONSTRAINT `fk_food_tb_main_tb` FOREIGN KEY (`main_tb_id`) REFERENCES `main_tb` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `food_tb`
+--
+
+LOCK TABLES `food_tb` WRITE;
+/*!40000 ALTER TABLE `food_tb` DISABLE KEYS */;
+INSERT INTO `food_tb` VALUES (1,1,0,1,0,1),(2,1,0,0,1,1),(3,1,0,0,0,0),(4,0,1,0,1,1);
+/*!40000 ALTER TABLE `food_tb` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-10-15 14:05:11
